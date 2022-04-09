c7a40898b8ff7ee3d1e80314b69cb784903aa247
Muhammad Salah <brokedeveloperhelp@gmail.com>
2022-04-10 01:09:15 +0200

    feat: Client-side: making a GET request to openweathermap api,Server-side: adding a GET request to get projectData object

--- server.js
+++ server.js
@@ -25,3 +25,11 @@ const port = 3000;
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
 });
+
+/**
+ * Server Routes
+ */
+
+app.get('/weather', (req, res) => {
+	return res.send(projectData);
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,23 @@
 /* Global Variables */
-
+const apiKey = '4b70fea52db751e99e5ba3673250f589&units=imperial';
+const baseURL = 'https://api.openweathermap.org/data/2.5';
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = `${d.getMonth()}.${d.getDate()}.${d.getFullYear()}`;
+
+const getWeather = async (countryCode = '', zip = '') => {
+	try {
+		//Please note if countryCode parameter is not specified then the search works for USA as a default.
+		const res = await fetch(
+			`${baseURL}/weather?zip=${zip},${countryCode}&appid=${apiKey}`
+		);
+
+		//Transform into json
+		const data = await res.json();
+
+		console.log(data);
+	} catch (error) {
+		console.log(error);
+	}
+};
+const weatherResponse = getWeather('us', 94040);
