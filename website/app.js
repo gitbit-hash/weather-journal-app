752e6269ea10f889fd4445e31687b026a97e0e1f
Muhammad Salah <brokedeveloperhelp@gmail.com>
2022-04-10 03:15:36 +0200

    refactor: refactoring getWeather function to accept zipcode, baseURL, and apiKey

--- website/app.js
+++ website/app.js
@@ -5,12 +5,10 @@ const baseURL = 'https://api.openweathermap.org/data/2.5';
 let d = new Date();
 let newDate = `${d.getMonth()}.${d.getDate()}.${d.getFullYear()}`;
 
-const getWeather = async (countryCode = '', zip = '') => {
+const getWeather = async (zip = '', baseURL = '', apiKey = '') => {
 	try {
 		//Please note if countryCode parameter is not specified then the search works for USA as a default.
-		const res = await fetch(
-			`${baseURL}/weather?zip=${zip},${countryCode}&appid=${apiKey}`
-		);
+		const res = await fetch(`${baseURL}/weather?zip=${zip},us&appid=${apiKey}`);
 
 		//Transform into json
 		const data = await res.json();
@@ -41,7 +39,7 @@ const postData = async (url = '', data = {}) => {
 };
 
 const getPostData = () => {
-	getWeather('us', 94040).then((data) => {
+	getWeather(94040, baseURL, apiKey).then((data) => {
 		postData('http://localhost:3000/weather', {
 			temperature: data.main.temp,
 			date: newDate,
